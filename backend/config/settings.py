d8f7078f6f6d8d8ad61e6743cb80cb4215d13e70
exesilvestre <exesilvestre@gmail.com>
2025-07-07 21:11:30 +0000

    env loaddotenv added

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -12,9 +12,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+dotenv.load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
