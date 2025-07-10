8f2491f5fd7e1b6b2c6089c832e6da31c8d363b5
leo.fragherazzi <leomartin.fragherazzi@cba.gov.ar>
2025-07-10 00:48:29 -0300

    feat: google authentication

--- backend/user_auth/urls.py
+++ backend/user_auth/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import UserRegistrationView
-from rest_framework_simplejwt.views import TokenObtainPairView
-
+from .views import GoogleLogin, UserRegistrationView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+    
 urlpatterns = [
-    path('register/', UserRegistrationView.as_view(), name='register'),
-    path('login-jwt/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/register/', UserRegistrationView.as_view(), name='register'),
+    path('auth/login/jwt', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/login/jwt/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('auth/login/google/', GoogleLogin.as_view(), name='google_login'),
 
 ]
