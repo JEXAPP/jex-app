6b04f8e43b514beda7f03f94ba18c3ec17426868
leo.fragherazzi <leomartin.fragherazzi@cba.gov.ar>
2025-07-10 00:58:33 -0300

    fix: urls slash fixes

--- backend/user_auth/urls.py
+++ backend/user_auth/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
     
 urlpatterns = [
     path('auth/register/', UserRegistrationView.as_view(), name='register'),
-    path('auth/login/jwt', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/login/jwt/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/login/jwt/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('auth/login/google/', GoogleLogin.as_view(), name='google_login'),
 
