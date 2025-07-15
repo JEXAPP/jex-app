1d701373e2c6992a47f6142503ceb651bf58a4ed
carlosibarguren <165290274+carlosibarguren@users.noreply.github.com>
2025-07-15 19:23:07 -0300

    Delete frontend/config.ts

--- frontend/config.ts
+++
@@ -1,9 +0,0 @@
-import Constants from 'expo-constants';
-
-export const config = {
-  apiBaseUrl: Constants.expoConfig?.extra?.API_BASE_URL || 'http://localhost:8000',
-  googleClientIdAndroid: Constants.expoConfig?.extra?.GOOGLE_CLIENT_ID_ANDROID || '242946374654-htkjh0r5mbaa0ds518mqe90k4og3lkh6.apps.googleusercontent.com',
-  googleClientIdIos: Constants.expoConfig?.extra?.GOOGLE_CLIENT_ID_IOS || '242946374654-q9df2f5rrdgmdv825b2clmgfr74pqq87.apps.googleusercontent.com',
-  googleApiKey: Constants.expoConfig?.extra?.GOOGLE_CLIENT_ID_IOS || 'AIzaSyCBfqlHMnCW0kIBQeruVZbdGUlxyYeHlDQ'
-};
-
