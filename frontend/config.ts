b18e926afe3fae400977077a4a8a950a0d538990
exesilvestre <exesilvestre@gmail.com>
2025-07-19 14:14:32 +0000

    adding deploy config

--- frontend/config.ts
+++ frontend/config.ts
@@ -3,7 +3,7 @@ import Constants from 'expo-constants';
 export const config = {
   apiBaseUrl: Constants.expoConfig?.extra?.API_BASE_URL,
   googleClientIdAndroid: Constants.expoConfig?.extra?.GOOGLE_CLIENT_ID_ANDROID,
-  googleClientIdIos: Constants.expoConfig?.extra?.GOOGLE_CLIENT_ID_IOS ,
-  googleApiKey: Constants.expoConfig?.extra?.GOOGLE_CLIENT_ID_IOS,
+  // googleClientIdIos: Constants.expoConfig?.extra?.GOOGLE_CLIENT_ID_IOS ,
+  // googleApiKey: Constants.expoConfig?.extra?.GOOGLE_CLIENT_ID_IOS,
   googleExpoClientId: Constants.expoConfig?.extra?.GOOGLE_EXPO_CLIENT_ID ,
 };
