d073b65be0195951e5dd4d06074f34b300533b07
leo.fragherazzi <leomartin.fragherazzi@cba.gov.ar>
2025-07-31 19:41:40 -0300

    feat: pagination and fix endpoint

--- backend/eventos/views/list_vacancy.py
+++ backend/eventos/views/list_vacancy.py
@@ -1,26 +1,20 @@
-from rest_framework.generics import RetrieveAPIView
+from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
 from eventos.models.event import Event
 from eventos.serializers.list_vacancy import EventWithVacanciesSerializer
 from user_auth.permissions import IsInGroup  # Suponiendo que la tenés definida
 from rest_framework.exceptions import NotFound
+from user_auth.constants import EMPLOYER_ROLE
 
-
-class EventVacanciesView(RetrieveAPIView):
+class EmployerEventsWithVacanciesView(ListAPIView):
     serializer_class = EventWithVacanciesSerializer
     permission_classes = [IsAuthenticated, IsInGroup]
-    required_groups = ["employer"]
+    required_groups = [EMPLOYER_ROLE]
 
-    def get_object(self):
+    def get_queryset(self):
         user = self.request.user
-        event_id = self.kwargs.get('event_id')
-
-        try:
-            return Event.objects.prefetch_related(
-                'vacancies',
-                'vacancies__job_type',
-                'vacancies__state'
-            ).get(id=event_id, owner=user)
-        except Event.DoesNotExist:
-            raise NotFound(detail="El evento no existe o no pertenece al usuario.")
+        return Event.objects.filter(owner=user).prefetch_related(
+            'vacancies__state',
+            'vacancies__job_type'
+        )
 
