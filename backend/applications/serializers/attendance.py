65f5bfe04974168c4b7f13e81a74ccb891c0266c
exesilvestre <exesilvestre@gmail.com>
2025-08-30 22:46:41 +0000

    adding validation

--- backend/applications/serializers/attendance.py
+++ backend/applications/serializers/attendance.py
@@ -1,9 +1,9 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import serializers
-
 from applications.errors.attendance_messages import (
     EMPLOYEE_NOT_FOUND,
     NOT_ACCEPTED_OFFER,
+    NOT_EVENT_OWNER,
     SHIFT_NOT_FOUND,
     ALREADY_REGISTERED_ATTENDANCE
 )
@@ -22,34 +22,45 @@ class AttendanceValidationSerializer(serializers.Serializer):
     def validate(self, attrs):
         shift_id = attrs.get("shift_id")
         employee_id = attrs.get("employee_id")
+        request_user = self.context['request'].user
 
-        # Validar que existan Shift y Employee
+        attrs["shift"] = self._validate_shift(shift_id)
+        attrs["employee"] = self._validate_employee(employee_id)
+        self._validate_offer(shift_id, employee_id)
+        self._validate_attendance_exists(shift_id, employee_id)
+        self._validate_event_owner(attrs["shift"], request_user)
+
+        return attrs
+
+    def _validate_shift(self, shift_id):
         try:
-            attrs["shift"] = Shift.objects.get(pk=shift_id)
+            return Shift.objects.get(pk=shift_id)
         except Shift.DoesNotExist:
             raise serializers.ValidationError(SHIFT_NOT_FOUND)
 
+    def _validate_employee(self, employee_id):
         try:
-            attrs["employee"] = EmployeeProfile.objects.get(pk=employee_id)
+            return EmployeeProfile.objects.get(pk=employee_id)
         except EmployeeProfile.DoesNotExist:
             raise serializers.ValidationError(EMPLOYEE_NOT_FOUND)
 
-        # Validar que el empleado tenga una oferta aceptada para ese turno
+    def _validate_offer(self, shift_id, employee_id):
         state_accepted = get_object_or_404(OfferState, name=OfferStates.ACCEPTED.value)
         has_offer = Offer.objects.filter(
             selected_shift_id=shift_id,
             employee_id=employee_id,
             state=state_accepted
         ).exists()
-
         if not has_offer:
             raise serializers.ValidationError(NOT_ACCEPTED_OFFER)
 
-        # Validar que no exista asistencia previamente registrada
+    def _validate_attendance_exists(self, shift_id, employee_id):
         if Attendance.objects.filter(employee_id=employee_id, shift_id=shift_id).exists():
             raise serializers.ValidationError(ALREADY_REGISTERED_ATTENDANCE)
 
-        return attrs
+    def _validate_event_owner(self, shift, user):
+        if shift.vacancy.event.owner != user:
+            raise serializers.ValidationError(NOT_EVENT_OWNER)
 
 
 class AttendanceResponseSerializer(serializers.ModelSerializer):
