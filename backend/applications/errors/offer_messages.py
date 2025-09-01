6dddd04da382f408e5db3a4433e51da9cacb94fe
valentin Rodriguez Saseta <valenrs2002@gmail.com>
2025-09-01 12:51:14 -0300

    Change error

--- backend/applications/errors/offer_messages.py
+++ backend/applications/errors/offer_messages.py
@@ -1,5 +1,3 @@
-
-
 # Employer
 EMPLOYER_PROFILE_NOT_FOUND = "El empleador no tiene perfil asociado."
 
@@ -10,3 +8,5 @@ INVALID_EXPIRATION_DATE = "Formato inválido. Usá DD/MM/YYYY."
 OFFER_NOT_PENDING = "Solo se pueden modificar ofertas en estado 'pending'."
 REJECTION_REASON_REQUIRED = "Debés especificar un motivo de rechazo si rechazás la oferta."
 NOT_PERMISSIONS_OFFER = "No podes accceder a esta oferta"
+
+MAX_OFFERS_REACHED = "No se pueden crear más ofertas para este turno. Cantidad máxima permitida: {max_quantity}."
