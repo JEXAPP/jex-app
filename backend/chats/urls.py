a8127789e805af0a9bbf0f1ae36d1984ead7d934
leo.fragherazzi <leomartin.fragherazzi@cba.gov.ar>
2025-09-29 23:04:47 -0300

    fix: url pattern

--- backend/chats/urls.py
+++ backend/chats/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from chats.views.chat import StreamTokenView
 
 
-url_patterns = [
+urlpatterns = [
     path('stream/token/', StreamTokenView.as_view(), name='stream_token'),
 ]
