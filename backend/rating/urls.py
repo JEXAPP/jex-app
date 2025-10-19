a93b04d53768d888cd8e298901b6e3f1c9bf1e77
exesilvestre <exesilvestre@gmail.com>
2025-10-19 12:49:12 +0000

    penalties

--- backend/rating/urls.py
+++ backend/rating/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rating.views.penalty import CreatePenaltyView, ListPenaltyCategoriesView
 from rating.views.rating import BulkCreateRatingView, ViewRatings, ListEmployersEventsView, BulkCreateEmployerRatingView
 
 urlpatterns = [
@@ -6,4 +7,7 @@ urlpatterns = [
     path('employers/torating/', ListEmployersEventsView.as_view(), name='list-employers-to-rating'),
     path('viewratings/<int:user_id>/', ViewRatings.as_view(), name='list-participation-employee'),
     path('rate-employer/', BulkCreateEmployerRatingView.as_view(), name='bulk-realize-employer-rating'),
+    path('penalty/categories/', ListPenaltyCategoriesView.as_view(), name='list-penalty-categories'),
+    path('penalty/create/', CreatePenaltyView.as_view(), name='create-penalty'),
+
 ]
